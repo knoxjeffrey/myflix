e2d7d3d64f835bacc54daa9c0dc556416384049d
Jeff Knox <knoxjeffrey@outlook.com>
2015-02-08 00:49:21 +0000

    moved root from videos to categories

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,9 +1,5 @@
 class VideosController < ApplicationController
   
-  def index
-    @categories ||= Category.all
-  end
-  
   def show
     @video ||= Video.find(params[:id])
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Myflix::Application.routes.draw do
   
-  get '/home', to: 'videos#index'
+  get '/home', to: 'categories#index'
   get 'ui(/:action)', controller: 'ui'
   
   resources :videos, only: [:show]
   resources :categories, only: [:show]
   
-  root to: 'videos#index'
+  root to: 'categories#index'
   
 end
