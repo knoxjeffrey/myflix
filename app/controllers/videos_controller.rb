25b2e5f88ce55b38baebded814c0ba0913a9e675
Jeff Knox <knoxjeffrey@outlook.com>
2015-02-09 09:52:36 +0000

    move video display method to model class. Dropped memoization for now and will implement when more stable

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,7 +1,7 @@
 class VideosController < ApplicationController
   
   def show
-    @video ||= Video.find(params[:id])
+    @video = Video.find(params[:id])
   end
   
 end
--- app/models/video.rb
+++ app/models/video.rb
@@ -5,4 +5,8 @@ class Video < ActiveRecord::Base
   validates :title, presence: true
   validates :description, presence: true
   
+  def display_large_video_image
+    self.large_cover_url.present? ? self.large_cover_url : "http://dummyimage.com/665x375/000000/00a2ff"
+  end
+  
 end
