f66c361843644bffb21be355b1875d879205099a
Jeff Knox <knoxjeffrey@outlook.com>
2015-02-09 15:50:05 +0000

    used validates presence of in models and add default order in category model

--- app/models/video.rb
+++ app/models/video.rb
@@ -2,8 +2,7 @@ class Video < ActiveRecord::Base
   
   belongs_to :category
   
-  validates :title, presence: true
-  validates :description, presence: true
+  validates_presence_of :title, :description
   
   def display_large_video_image
     self.large_cover_url.present? ? self.large_cover_url : "http://dummyimage.com/665x375/000000/00a2ff"
