a9dce3ce1facb4eb0888de54bf08a23c51c3346c
Jeff Knox <knoxjeffrey@outlook.com>
2015-02-25 17:54:40 +0000

    refactor user sign in feature test

--- spec/features/user_signs_in_spec.rb
+++ spec/features/user_signs_in_spec.rb
@@ -6,14 +6,26 @@ feature "user signs in" do
   
   scenario "with existing email and correct password" do
     sign_in_user(valid_user)
-    expect(page.find('.dropdown #dlabel').text).to have_content valid_user.full_name
+    expect_drop_down_to_contain_full_name(valid_user)
   end
   
   scenario "with incorrect login details" do
     visit sign_in_path
-    fill_in "Email Address", with: valid_user.email_address
-    fill_in "Password", with: "totally wrong password"
+    enter_incorrect_login_details(valid_user)
     click_button "Sign In"
+    expect_to_return_to_sign_in_page_and_see_error
+  end
+  
+  def expect_drop_down_to_contain_full_name(user)
+    expect(page.find('.dropdown #dlabel').text).to have_content user.full_name
+  end
+  
+  def enter_incorrect_login_details(user)
+    fill_in "Email Address", with: user.email_address
+    fill_in "Password", with: "totally wrong password"
+  end
+  
+  def expect_to_return_to_sign_in_page_and_see_error
     expect(current_path).to eq(sign_in_path)
     expect(page).to have_content "There is a problem with your username or password"
   end
