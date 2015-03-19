3a877b28a9ee649b71ba841e15591c5ffa4c64e6
Jeff Knox <knoxjeffrey@outlook.com>
2015-03-19 23:36:46 +0000

    cicle file and bullet

--- Gemfile
+++ Gemfile
@@ -22,6 +22,7 @@ group :development do
   gem "better_errors"
   gem "binding_of_caller"
   gem 'letter_opener'
+  gem "bullet"
 end
 
 group :development, :test do
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -20,4 +20,9 @@ Myflix::Application.configure do
   config.assets.debug = true
 
   config.eager_load = false
+  
+  config.after_initialize do
+    Bullet.enable = true
+    Bullet.alert = true
+  end
 end
