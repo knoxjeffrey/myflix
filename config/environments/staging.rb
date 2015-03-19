f8fc82cb77d906359a968480e721779f5984a5fd
Jeff Knox <knoxjeffrey@outlook.com>
2015-03-19 11:05:14 +0000

    staging environment

---
+++ config/environments/staging.rb
@@ -0,0 +1,31 @@
+Myflix::Application.configure do
+
+  config.cache_classes = true
+  config.eager_load = true
+  config.consider_all_requests_local       = false
+  config.action_controller.perform_caching = true
+
+  config.serve_static_assets = false
+
+  config.assets.compress = true
+  config.assets.js_compressor = :uglifier
+
+  config.assets.compile = false
+
+  config.assets.digest = true
+
+  config.i18n.fallbacks = true
+
+  config.active_support.deprecation = :notify
+  
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.mandrillapp.com",
+    :port                 => 587,
+    :user_name            => ENV['MANDRILL_USERNAME'],
+    :password             => ENV['MANDRILL_APIKEY'],
+    :domain               => 'heroku.com',
+    :authentication       => 'plain'
+  }
+  config.action_mailer.default_url_options = { host: 'http://knoxjeffrey-myflix.herokuapp.com/' }
+end
