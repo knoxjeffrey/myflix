01f07ea0b0b9ddaaa565d6a66f7273544196b115
Jeff Knox <knoxjeffrey@outlook.com>
2015-03-23 22:42:21 +0000

    watch video

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,15 +1,16 @@
 CarrierWave.configure do |config|
-  if Rails.env.staging? || Rails.env.productions?
-    config.storage = :fog
-    config.fog_credentials = {
-      provider:              'AWS',                           # required
-      aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],        # required
-      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],    # required
-      region:                'eu-west-1'
-    }
-    config.fog_directory  = ENV['AWS_S3_BUCKET']              # required
+  config.storage = :fog
+  config.fog_credentials = {
+    provider:              'AWS',
+    aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
+    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+    region:                ENV['AWS_REGION']
+  }
+  if Rails.env.production?
+    config.fog_directory  = ENV['AWS_S3_BUCKET_PRODUCTION']
+  elsif Rails.env.staging?
+    config.fog_directory  = ENV['AWS_S3_BUCKET_STAGING']
   else
-    config.storage = :file  
-    config.enable_processing = Rails.env.development?                             
+    config.fog_directory  = ENV['AWS_S3_BUCKET_DEVELOPMENT']
   end
 end
