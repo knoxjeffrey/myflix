e43b80038543f3c701653ab0ec40f2b0dfcb875e
Jeff Knox <knoxjeffrey@outlook.com>
2015-05-27 14:27:08 +0100

    tidy up of app

--- app/mailers/app_mailer.rb
+++ app/mailers/app_mailer.rb
@@ -1,12 +1,12 @@
 class AppMailer < ActionMailer::Base
   def notify_on_user_signup(user)
     @user = user
-    mail from: 'info@tamars.co.uk', to: user.email_address, subject: "Welcome to MyFLiX"
+    mail from: 'knoxjeffrey@outlook.com', to: user.email_address, subject: "Welcome to MyFLiX"
   end
   
   def send_forgot_password(user)
     @user = user
-    mail from: 'info@tamars.co.uk', to: user.email_address, subject: "Reset Password"
+    mail from: 'knoxjeffrey@outlook.com', to: user.email_address, subject: "Reset Password"
   end
   
   def send_invite(invitation)
